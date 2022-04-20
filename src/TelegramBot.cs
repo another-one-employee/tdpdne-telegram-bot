fa9031e26d92550b0a3dc03bd5d8bd8ff9d09526
Srul1k <72085557+Srul1k@users.noreply.github.com>
2022-04-20 17:22:07 +0300

    Initialize project

---
+++ src/TelegramBot.cs
@@ -0,0 +1,24 @@
+﻿using Telegram.Bot;
+
+namespace XMinusBot
+{
+    public class TelegramBot
+    {
+        private readonly IConfiguration _configuration;
+
+        public TelegramBot(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<TelegramBotClient> GetBot()
+        {
+            var telegramBot = new TelegramBotClient(_configuration["Token"]);
+
+            var hook = $"{_configuration["Url"]}/api/message/ping";
+            await telegramBot.SetWebhookAsync(hook);
+
+            return telegramBot;
+        }
+    }
+}
